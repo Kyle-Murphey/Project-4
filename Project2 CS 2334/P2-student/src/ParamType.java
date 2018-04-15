d9d84417ca171d899cafd6ebe7883a259be9fb4e
Kyle-Murphey <kyle_murphey@live.com>
2018-04-15 00:05:54 -0500

    Added enum ParamType

---
+++ Project2 CS 2334/P2-student/src/ParamType.java
@@ -0,0 +1,17 @@
+/***
+ * 
+ * @author Kyle Murphey
+ * @version 2018/4/14
+ * 
+ * Parameter names (TAIR, TA9M, SRAD)
+ *
+ */
+public enum ParamType
+{
+    /** tair name **/
+    TAIR,
+    /** ta9m name **/
+    TA9M,
+    /** solar radiation name **/
+    SRAD
+}
